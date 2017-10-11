164b5ccc2db4ebab6a8d9c31dc3278541c8f8bc5
Ludo Siennat <ludovic.siennat@gmail.com>
2017-10-11 19:45:14 +0200

    Last tests

--- tests/TicketBundle/Controller/TicketingControllerTest.php
+++ tests/TicketBundle/Controller/TicketingControllerTest.php
@@ -25,32 +25,13 @@ class TicketingControllerTest extends WebTestCase
 
         $this->assertEquals(1, $crawler->filter('h1:contains("Selection des Billets ")')->count());
 
-        $form = $crawler->selectButton('Etape suivante')->form();
 
-        $values = $form->getPhpValues();
+    }
+    public function testNotValidData()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/recaptilatif/{id}');
 
-
-        $values['louvre_ticketbundle_globalticket[ticketype]'] = 'Journée';
-        $values['louvre_ticketbundle_globalticket[mail]'] = 'ludovic.siennat@gmail.com';
-//        $values['louvre_ticketbundle_globalticket[datevisit]'] = new \DateTime('1991-02-06');
-
-        $values['louvre_ticketbundle_globalticket']['tickets'][0]['name'] = "siennat";
-        $values['louvre_ticketbundle_globalticket']['tickets'][0]['firstname'] = "ludodic";
-        $values['louvre_ticketbundle_globalticket']['tickets'][0]['country'] = 'france';
-//        $values['louvre_ticketbundle_globalticket']['tickets'][0]['birthdate'] = array(
-//            'year' => 1991,
-//            'month' => 03,
-//            'day' => 06);
-
-
-        $crawler = $client->request($form->getMethod(), $form->getUri(), $values, $form->getPhpFiles());
-        $crawler = $client->submit($form);
-//
-        var_dump($client->getResponse());
-
-
-
-         $client->followRedirect();
-//        echo ->getContent();
+        $this->assertFalse($client->getResponse()->isSuccessful());
     }
 }
